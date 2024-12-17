f1e68123ef98b718293bee6f39b58f073113eac5
Andrey B <TakeItEasyO@yandex.com>
2024-12-17 19:38:17 +0300

    Updating base model

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
+from config import settings
+
+from datetime import datetime
 from typing import AsyncGenerator
 
+from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, declared_attr
-
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from app.config import settings
 from utils.case_converter import camel_case_to_snake_case
 
 
@@ -13,6 +16,9 @@ class Base(DeclarativeBase):
     @declared_attr
     def __tablename__(cls):
         return f'{camel_case_to_snake_case(cls.__name__.lower())}s'
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_ad: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
 
 
 class DatabaseHelper:
